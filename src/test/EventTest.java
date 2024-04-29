cad8281654c2c82c78ad4487aa891dc78afd1b5a
Impolex <117767293+Impolex@users.noreply.github.com>
2024-04-29 20:15:41 +0200

    Fixed tests for event class

--- src/test/EventTest.java
+++ src/test/EventTest.java
@@ -10,30 +10,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EventTest {
 
-    User user1;
-    User test;
-    Event event;
+    User user1 = null;
+    User test = null;
+    Event event = null;
 
     @BeforeEach
     void prepare() {
-        System.out.println("beforeeach");
         user1 = new User();
         user1.setUserId(1);
         user1.setUserName("user1");
-        System.out.println("user created");
 
-        User test = new User();
-        test.setUserName("testuser");
+        test = new User();
         test.setUserId(999);
-        System.out.println("testuser created");
+        test.setUserName("testuser");
 
         event = new Event(user1, "Crying violently", "I dont fucking know anymore",
             "My basement", "01.01.2000, 12:00");
-        System.out.println("event created");
-
         event.addParticipator(test);
-        System.out.println("testuser added");
-        System.out.println(event.getParticipants().contains(test));
     }
 
     @AfterEach
