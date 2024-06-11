fbe79ed8c76d49ea19ff7d1d18983d790986aa65
BratenDackel <heckel.julian.a22@student.dhbw-karlsruhe.de>
2024-06-11 19:47:44 +0200

    Importet JavaFx

---
+++ src/main/java/ui/test.java
@@ -0,0 +1,12 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class test extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
