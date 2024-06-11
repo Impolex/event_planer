b3f58066dc1c5a630263d0b557e6f58da0090386
Julian Heckel <julian.heckel@siemens-energy.com>
2024-06-11 19:55:34 +0200

    Added fxml

--- src/main/java/ui/test.java
+++ src/main/java/ui/test.java
@@ -1,12 +1,19 @@
 package ui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class test extends Application {
+    public test() throws IOException {
+    }
 
     @Override
     public void start(Stage stage) throws Exception {
 
     }
+    FXMLLoader loader = FXMLLoader.load(getClass().getResource("eventspage-view.fxml"));
+
 }
