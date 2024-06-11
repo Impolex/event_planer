be151bec382f8d4ef1932460f28264a9c1e82bcb
BratenDackel <heckel.julian.a22@student.dhbw-karlsruhe.de>
2024-06-11 20:07:08 +0200

    simple Scene

--- src/main/java/ui/test.java
+++ src/main/java/ui/test.java
@@ -2,18 +2,19 @@ package ui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class test extends Application {
-    public test() throws IOException {
-    }
+
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        Parent loader = FXMLLoader.load(getClass().getResource("eventspage-view.fxml"));
+        stage.setScene(new Scene(loader, 600, 400));
+        stage.show();
     }
-    FXMLLoader loader = FXMLLoader.load(getClass().getResource("eventspage-view.fxml"));
-
 }
