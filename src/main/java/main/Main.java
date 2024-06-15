e0242be6a4ca0230cf83bda7539886fc75152a13
Julian Heckel <julian.heckel@siemens-energy.com>
2024-06-15 18:22:56 +0200

    Fixed issue with main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,3 +1,5 @@
+package main;
+
 import classes.Event;
 import classes.User;
 
@@ -7,7 +9,6 @@ public class Main {
         User user1 = new User();
         user1.setUserId(1);
         user1.setUserName("user1");
-
         Event event = new Event(user1, "Crying violently", "I dont fucking know anymore",
                 "My basement", "01.01.2000, 12:00");
     }
